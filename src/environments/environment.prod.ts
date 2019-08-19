d32c003448dfb492c3420db0ae828df965dddb86
Autumn <erin.a.henderson1@gmail.com>
2019-08-19 13:50:13 -0400

    ah

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
   production: true
 };
+export let APIURL = '';
+
+switch (window.location.hostname) {
+    // this is the deployed angular application
+    case 'https://recipeangularstore.herokuapp.com':
+        // this is the full url of your deployed API
+        APIURL = ''
+        break;
+    default:
+        // this is the local host name of your API
+        APIURL = 'http://localhost:3000';
+}
